2abf70563138f2310e9dc7eb6da62c269d5cd61f
marioayala15 <mario.ayala.valenzuela@gmail.com>
2021-06-11 11:44:30 +0200

    some cleaning

--- Pygamevectors.py
+++ Pygamevectors.py
@@ -1,4 +1,6 @@
 import pygame, sys, random	
+from uuid import uuid4
+import ctypes
 
 #Colours 
 white=(255,255,255)
@@ -7,15 +9,15 @@ green=(0,255,0)
 red=(255,0,0)
 
 
-# Simulation model-parameters 
+# Model-parameters 
 
 wind_withd = 500
 wind_height = 500
 a=0
-sigma=1
+sigma=5
 dt=1
 
-n = 5000 # number of vectors (they dont die during the whole simulation)
+n = 1000 # number of vectors (they dont die during the whole simulation)
 
 pygame.init()
 screen = pygame.display.set_mode((wind_withd,wind_height))
@@ -26,19 +28,42 @@ clock = pygame.time.Clock()
 
 screen.fill(white)
 
+########    Recurrent functions  ######
 
+# This function finds an object via the objects id 
+def find_object(list,identifier):
+    for element in list:
+        if element.id == identifier:
+            return element
+
+
+#Clases
 class vector(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
+        self.id = str(uuid4())
         self.size = 4
         self.xpos = x
         self.ypos = y 
+        self.trait = 0   #  Taking values in [0,1] ( initially not carrying viruses) 
+        self.color = (255*self.trait,0,0)    #Black is healthy,  255 red is infected (in betweeen traits)
         self.image = pygame.Surface([self.size, self.size])
-        self.image.fill(red)
+        self.image.fill(self.color)
         self.rect =self.image.get_rect()
         self.rect.center=(self.xpos,self.ypos)
-        self.state = 0 
-        
+         
+    def load(self,contag_trait):
+        if self.trait==0:
+            self.trait= contag_trait 
+            self.color = (255*self.trait,0,0) 
+            self.image.fill(self.color)
+
+    def unload(self):
+        if self.trait>0:
+            self.trait= 0
+            self.color = (255*self.trait,0,0) 
+            self.image.fill(self.color)
+
 
     def update(self):
         self.xpos = self.xpos + a*dt + sigma*random.normalvariate(0,dt)
@@ -58,33 +83,66 @@ class vector(pygame.sprite.Sprite):
 class plant(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
+        self.id = str(uuid4())
         self.xpos = x
         self.ypos = y 
-        self.image = pygame.Surface([8, 8])
+        self.size = 2
+        self.image = pygame.Surface([self.size, self.size])
         self.image.fill(green)
         self.rect =self.image.get_rect()
         self.rect.center=(self.xpos,self.ypos)
-        self.state = 0 
+        self.trait = []  # List of viruses (inititally empty) and its traits  (in [0,1])
+
+    def infection(self,contag_trait):
+        self.size += 2
+        self.image = pygame.Surface([self.size, self.size])
+        self.image.fill(green)
+        self.trait.append(contag_trait)
+
+    def clonebirth(self,parent_trait):
+        self.size+=2
+        self.image = pygame.Surface([self.size, self.size])
+        self.image.fill(green)
+        self.trait.append(parent_trait)
 
 
 
 
 vectors_group = pygame.sprite.Group()
 plants_group = pygame.sprite.Group()
+infected_plants_group = pygame.sprite.Group()
+vectors_id=[]
+plants_id =[]
+infected_plants_id =[]
 
 for i in range(n):
-    vectors_group.add(vector(random.randint(0,wind_withd),random.randint(0,wind_height)))
+    mosco = vector(random.randint(0,wind_withd),random.randint(0,wind_height))
+    vectors_group.add(mosco)
+    vectors_id.append(mosco.id)
 
-
-#pygame.draw.rect(screen,white,(100,100,300,300))
 
 
 for i in range(105,400,10):
     for j in range(105,400,10):
-        plants_group.add(plant(i,j))
+        planta=plant(i,j)
+        plants_group.add(planta)
+        plants_id.append(planta.id)
 
 
+#Let us load some vectos with viruses
+p= 0.4  #Proportion of loaded vectors at time zero
+charged_vectors_group = pygame.sprite.Group()
+charged_vectors_id =[]   #Empty list with ids
 
+for mosco in vectors_group:
+    unif= random.random()
+    new_trait= random.random()  #we pick the new trait at random
+    if unif <= p:
+        mosco.load(new_trait)
+        charged_vectors_group.add(mosco)
+        charged_vectors_id.append(mosco.id)
+        vectors_group.remove(mosco)
+        vectors_id.remove(mosco.id)
 
 
 
@@ -94,10 +152,47 @@ while True:
             pygame.quit()
             sys.exit()
 
+    theta = random.random  #Unif [0,1] random variable to decide the type of event
+
+    ###############################
+    ####### Types of events #######
+    ###############################
+
+    ##### Birth of a virus in a plant ########  (BETA version, is not following the models logic)
+    if len(infected_plants_id)>0:
+        target_plant_id=random.choice(infected_plants_id)
+
+        target_plant = find_object(infected_plants_group,target_plant_id)
+        target_plant_trait= random.choice(target_plant.trait)   #selecting new trait (for the moment doesnt make sense)
+
+        target_plant.clonebirth(target_plant_trait)
+
+    #####  Unloading of virus from vector to plant ######
+    if len(charged_vectors_id) > 0:
+        target_mosco_id = random.choice(charged_vectors_id)
+
+        target_mosco=find_object(charged_vectors_group,target_mosco_id)
+
+        for planta in plants_group:
+            if pygame.sprite.collide_rect_ratio(1)(target_mosco, planta):
+                planta.infection(target_mosco.trait)
+                
+
+                infected_plants_id.append(planta)
+
+                infected_plants_group.add(planta)
+                target_mosco.unload()
+                vectors_group.add(target_mosco)
+                vectors_id.append(target_mosco_id)
+                charged_vectors_group.remove(target_mosco)
+                charged_vectors_id.remove(target_mosco_id)
+
     screen.fill(white)
     plants_group.draw(screen)
     vectors_group.update()
     vectors_group.draw(screen)
+    charged_vectors_group.update()
+    charged_vectors_group.draw(screen)
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(60)
 
